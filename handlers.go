d8b9aba85f10207767712782406f41cbe84c3086
AshrafKhan736 <gdak9121997@gmail.com>
2025-01-13 09:43:21 +0530

    Added the logic to sensor profane words
    Changed the api paths
    Refactored the code

--- handlers.go
+++ handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+	"strings"
 	"sync/atomic"
 )
 
@@ -33,13 +35,95 @@ func (cfg *apiConfig) getMetrics(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(200)
 	headers := w.Header()
 	headers.Set("content-type", "text/plain; charset=utf-8")
-	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileServerHits.Load()))))
+	response := fmt.Sprintf(`
+    <html>
+      <body>
+        <h1>Welcome, Chirpy Admin</h1>
+        <p>Chirpy has been visited %d times!</p>
+      </body>
+    </html>`, cfg.fileServerHits.Load())
+	w.Write([]byte(response))
 }
 
-func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, _ *http.Request) {
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	headers := w.Header()
 	headers.Set("content-type", "text/plain; charset=utf-8")
 	cfg.fileServerHits.Store(0)
 	w.Write([]byte("Hit reset to 0"))
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.WriteHeader(code)
+	data, err := json.Marshal(struct {
+		Error string `json:"error"`
+	}{Error: msg})
+	if err != nil {
+		w.WriteHeader(500)
+		log.Printf("Error in parsing json %s \n", err)
+		return
+	}
+	w.Write(data)
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	w.WriteHeader(code)
+	data, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(500)
+		log.Printf("Error in parsing json %s \n", err)
+		return
+	}
+	w.Write(data)
+}
+
+func sliceToMap(slice []string) map[string]bool {
+	result := make(map[string]bool)
+	for _, item := range slice {
+		result[item] = true
+	}
+	return result
+}
+
+func cleanChirp(msg string) string {
+	profaneWords := []string{"kerfuffle", "sharbert", "fornax"}
+	wordMap := sliceToMap(profaneWords)
+	result := []string{}
+	for _, word := range strings.Split(msg, " ") {
+		if wordMap[strings.ToLower(word)] {
+			result = append(result, "****")
+		} else {
+			result = append(result, word)
+		}
+	}
+	return strings.Join(result, " ")
+}
+
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	headers := w.Header()
+	headers.Set("content-type", "application/json")
+
+	chirp := struct {
+		Body string `json:"body"`
+	}{}
+	decoder := json.NewDecoder(r.Body)
+
+	err := decoder.Decode(&chirp)
+	if err != nil {
+		log.Println("Error decoding response body " + err.Error())
+		respondWithError(w, 500, err.Error())
+		return
+	}
+
+	if len([]rune(chirp.Body)) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	payload := struct {
+		CleanedBody string `json:"cleaned_body"`
+	}{}
+
+	payload.CleanedBody = cleanChirp(chirp.Body)
+	respondWithJson(w, 200, payload)
 }
--- main.go
+++ main.go
@@ -11,9 +11,10 @@ func main() {
 	apiMertic := apiConfig{}
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiMertic.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(dir)))))
-	mux.HandleFunc("/healthz", healthCheck)
-	mux.HandleFunc("/metrics", apiMertic.getMetrics)
-	mux.HandleFunc("/reset", apiMertic.resetMetrics)
+	mux.HandleFunc("GET /api/healthz", healthCheck)
+	mux.HandleFunc("GET /admin/metrics", apiMertic.getMetrics)
+	mux.HandleFunc("POST /admin/reset", apiMertic.resetMetrics)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
 
 	srv := &http.Server{
 		Addr:    port,
